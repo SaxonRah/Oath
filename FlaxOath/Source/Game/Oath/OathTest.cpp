72ced08bd80631034dd14da8a5e75565e3f08406
Robert Valentine <paraboliclabs@gmail.com>
2025-03-17 22:43:44 -0400

    FlaxEngine Commit

---
+++ FlaxOath/Source/Game/Oath/OathTest.cpp
@@ -0,0 +1,25 @@
+﻿#include "OathTest.h"
+#include "Engine/Core/Log.h"
+
+OathTest::OathTest(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+void OathTest::OnEnable()
+{
+    // Here you can add code that needs to be called when script is enabled (eg. register for events)
+}
+
+void OathTest::OnDisable()
+{
+    // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+}
+
+void OathTest::OnUpdate()
+{
+    // Here you can add code that needs to be called every frame
+    LOG(Info, "Hello from C++!");
+}
